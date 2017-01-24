60859413237cbbecf9dc2ad2f3ad47513f0b15df
sylvia villegas <sylvia@svillegas.com>
2017-01-24 15:19:18 -0500

    Move HTML to own file

--- server.rb
+++ server.rb
@@ -15,17 +15,8 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  response = "
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>My first web server</title>
-      </head>
-      <body>
-        <h1>My first web server</h1>
-        <p>Oh hey, this is my first HTML response!</p>
-      </body>
-    </html>"
+  filename = "index.html"
+  response = File.read(filename)
 
   client.puts(response)
 
